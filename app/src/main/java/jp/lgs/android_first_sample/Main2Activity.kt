71de1a1ab65ac7d8c755ffba0a4127ce65846d18
kuroyan <m_kurosaki@mtf.biglobe.ne.jp>
2020-03-25 11:24:03 +0900

    Form2への移動とそこからの戻りボタンを機能させた

--- app/src/main/java/jp/lgs/android_first_sample/Main2Activity.kt
+++ app/src/main/java/jp/lgs/android_first_sample/Main2Activity.kt
@@ -1,12 +1,53 @@
 package jp.lgs.android_first_sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
     }
+
+
+    fun onClickReturnForm1(view: View) {
+        //--　以下どちらでも機能する。ステータスとすればどうなのか？
+
+        //val intent = Intent(applicationContext,MainActivity::class.java)
+        //startActivity(intent)
+
+        finish()
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
--- app/src/main/java/jp/lgs/android_first_sample/MainActivity.kt
+++ app/src/main/java/jp/lgs/android_first_sample/MainActivity.kt
@@ -1,12 +1,51 @@
 package jp.lgs.android_first_sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun toForm2Click( view: View)
+    {
+        val intent = Intent(applicationContext,Main2Activity::class.java)
+        //-- toastは以下でもstartActivityの後でも表示タイミングは関係ないようだ
+        //toastMake("画面変更中！！",0,-100)
+        startActivity(intent)
+        Id_form1Txtt.setText(R.string.helloAgain)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
